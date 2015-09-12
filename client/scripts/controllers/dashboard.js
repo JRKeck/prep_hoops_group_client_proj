efd4895db5d31e6be40f1a35470f730e05154b22
sdoire <sarah.doire@gmail.com>
2015-09-12 15:45:42 -0500

    adds redirect to admin page

--- client/scripts/controllers/dashboard.js
+++ client/scripts/controllers/dashboard.js
@@ -1,7 +1,15 @@
-prepHoopsApp.controller('DashboardController', ['$scope', '$http', function($scope, $http){
+prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', function($scope, $http, $location){
     $scope.sites = ["Site1", "Site2"];
     $scope.dates = [{date: "9/11/15", sites:[{Site1:[{articles:[{title: "Hoops", author: "Sarah"}, {title: "Hoops2", author: "Suren"}]}], Site2:[{articles:[{title: "Hoops3", author: "Josh"}]}]}]}];
 
+    $scope.go = function ( path ) {
+        console.log('clicked!');
+        $location.path( path );
+    };
+
+    $scope.click = function(){
+        console.log('clicked!');
+    };
 
 
 }]);
