775ee66943eb705219563b34aebdcef92001c800
sdoire <sarah.doire@gmail.com>
2015-09-12 14:55:50 -0500

    begins main dashboard creation

--- client/scripts/controllers/dashboard.js
+++ client/scripts/controllers/dashboard.js
@@ -1,4 +1,7 @@
 prepHoopsApp.controller('DashboardController', ['$scope', '$http', function($scope, $http){
-    console.log("Dashboard Controller Loaded");
+    $scope.sites = ["Site1", "Site2"];
+    $scope.dates = [{date: "9/11/15", sites:[{Site1:[{articles:[{title: "Hoops", author: "Sarah"}, {title: "Hoops2", author: "Suren"}]}], Site2:[{articles:[{title: "Hoops3", author: "Josh"}]}]}]}];
+
+
 
 }]);
