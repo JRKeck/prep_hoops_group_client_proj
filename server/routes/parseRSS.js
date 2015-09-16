b99439e900d22802bba637f9cb2a94c99d67d28b
Bob Fix <bobfix@gmail.com>
2015-09-16 16:21:21 -0500

    parseRSS and parseAPI connected - troubleshooting async issue

--- server/routes/parseRSS.js
+++ server/routes/parseRSS.js
@@ -7,6 +7,9 @@ var saveArticle = require('./parseAPI');
 // Keep track of the # of articles parsed
 var articleCount = 0;
 
+// Initializes an array that will hold the parsed objects
+var holdingArray = [];
+
 // Demo data of a req to the DB for all the sites
 var networkArray = [
     {
@@ -14,12 +17,12 @@ var networkArray = [
         shortName: 'MN',
         siteID: 1,
         siteFeed: 'http://www.northstarhoopsreport.com/news_rss_feed?tags=903525%2C477068%2C477064%2C718293%2C744134%2C744381%2C763955%2C744167%2C876578%2C454209%2C744386%2C744387%2C1513588%2C1469282'
-    //},
-    //{
-    //    siteName: 'Prep Hoops Iowa',
-    //    shortName: 'IA',
-    //    siteID: 2,
-    //    siteFeed: 'http://www.prephoopsiowa.com/news_rss_feed?tags=1160474%2C1160478%2C1160479%2C1160453%2C1164934%2C1164913%2C1164890%2C1164908%2C1161834%2C1330622'
+    },
+    {
+        siteName: 'Prep Hoops Iowa',
+        shortName: 'IA',
+        siteID: 2,
+        siteFeed: 'http://www.prephoopsiowa.com/news_rss_feed?tags=1160474%2C1160478%2C1160479%2C1160453%2C1164934%2C1164913%2C1164890%2C1164908%2C1161834%2C1330622'
     }
 ];
 
@@ -86,13 +89,15 @@ function parseFeed(feedURL, siteName, siteID){
                 articleObj.articleID = articleID;
 
                 //console.log("This is from parseRSS: ", articleObj);
-                saveArticle(articleObj);
-
+                holdingArray.push(articleObj);
+                console.log("Holding Array Items: ", holdingArray.length);
                 articleCount++;
                 console.log(articleCount + ' articles parsed');
             }
         });
     });
+    //saveArticle(holdingArray);
+
 }
 // Convert a date to ISO format
 function dateToISO(date){
