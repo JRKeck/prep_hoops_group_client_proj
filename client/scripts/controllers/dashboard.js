51ae0bef6991a6895a8447cf9e9b5ae32d040e6a
suren-vijayakumar <suren.vijayakumar@gmail.com>
2015-09-17 21:10:57 -0500

    dashboard stuff

--- client/scripts/controllers/dashboard.js
+++ client/scripts/controllers/dashboard.js
@@ -12,18 +12,18 @@ prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', 
     //Hard-coded data for testing purposes
     $scope.sites = [];
     $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    $scope.siteIds = [[1, []], [2, []], [3, []]];
+    //$scope.siteIds = [[1, []], [2, []], [3, []]];
+    $scope.siteIds = [1,2,3,4,5,6];
     $scope.daysArray = [];
     $scope.dates = [];
     $scope.firstDate = '';
     $scope.secondDate = '';
     $scope.dateRangeLength = 0;
 
-
     //Not yet working to get day of the week for specified date
     $scope.getDayOfWeek = function(date){
             $scope.dayOfWeek = date.getDay();
-            console.log($scope.dayOfWeek);
+            //console.log($scope.dayOfWeek);
     };
 
     //Function to make admin button redirect to admin page
@@ -45,7 +45,6 @@ prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', 
 
         $http.post('/api/articleGet', [$scope.firstDate, $scope.secondDate]).
             success(function(data){
-            //console.log(data);
                 $scope.dates = data;
                 $scope.sites = data[0].site;
                 $scope.dateArrayCreator();
@@ -59,8 +58,11 @@ prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', 
         for(var j=0; j<n;j++){
             $scope.siteArray.push([j+1,[]]);
         }
-    }
- $scope.sitesArrayCreator();
+    };
+
+
+    $scope.sitesArrayCreator();
+    //console.log($scope.siteArray);
     $scope.dateArrayCreator = function(){
 
         for(var i=0; i<4; i++ ) {
@@ -74,40 +76,41 @@ prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', 
 
 
  var dateRange= 4;
-    var temp;
+
     $scope.daySiteSort = function(){
-        console.log($scope.daysArray);
 
-        for (var i = 0; i < dateRange; i++){
-          console.log('from database: ' + $scope.dates[i].date);
-          if($scope.dates[i].date!=null){
-              if (($scope.daysArray[i][0] === $scope.dates[i].date)) {
-                  console.log('one date match');
-                  console.log($scope.daysArray[i][0]);
-                  //console.log($scope.dates[i].date);
+              for(var m =0; m < $scope.dates.length; m++) {
+                  for (var i = 0; i < dateRange; i++){
+                  if (($scope.daysArray[i][0] === $scope.dates[m].date)) {
+                      console.log("user generated " +$scope.daysArray[i][0] + "from dbase " + $scope.dates[m].date );
+                      for (var j = 0; j < $scope.daysArray[i][1].length; j++) {
+                          console.log("sites array" + $scope.dates[m].site);
+                           for (var k = 0; k < $scope.dates[m].site.length; k++) {
+                               if ($scope.daysArray[i][1][j][0] === $scope.dates[m].site[k].siteID) {
+                                   console.log("siteID position user gen " + $scope.daysArray[i][1][j][0]);
+                                   console.log("siteID position from db " + $scope.dates[m].site[k].siteID);
 
-                  for(var j = 0; j < $scope.dates[i].site.length; j++) {
-                      temp = $scope.dates[i].site[j];
-
-                      if($scope.daysArray[i][1][j][0] === temp.siteID){
-
-                          $scope.daysArray[i][1][j][1].push('this works');
+                                   $scope.daysArray[i][1][j][1].push($scope.dates[m].site[k].articles);
+                                   console.log("articles array posiitin within sites Array " + $scope.daysArray[i][1][j][1]);
+                                   console.log("articles from db " + $scope.dates[m].site[k].articles);
+                               }
+                           }
                       }
-                      console.log($scope.daysArray);
-
                   }
+                  //else {
+                  //$scope.daysArray[i][1].push(0);
+                  //    console.log("user generated " +$scope.daysArray[i][0]);
+                  //    console.log($scope.daysArray[i][1]);
+                  //}
               }
 
-          }
-
-
       }
-        //console.log($scope.daysArray);
+        console.log($scope.daysArray);
 
     };
 
     $scope.getArticles = function(){
-      console.log(this.day.site);
+      //console.log(this.day.site);
         for(var i=0; i < this.day.site[0].articles.length; i++){
             console.log(this.day.site[0].articles[i].url)
         }
@@ -142,83 +145,83 @@ prepHoopsApp.controller('DashboardController', ['$scope', '$http', '$location', 
 
     // This is Bob's test code for creating new database records.
     // All of this is not needed once we being parsing data.
-    var myDate = new Date('9/13/2015');
-    console.log(myDate);
-    var year = myDate.getFullYear().toString();
-    var tempMonth = myDate.getMonth();
-    var month = (tempMonth + 1).toString();
-    var day = myDate.getDate().toString();
-    var dbDate = year + month + day;
-    console.log(dbDate);
-
-    $scope.testDatabase = function() {
-        var newDateArticle =
-        {
-            date: dbDate,
-            site:
-                [{
-                    siteName: 'North Star Hoops Report',
-                    siteID: 1,
-                    articles:
-                        [{
-                            pubDate: 'Sat, 27 Dec 2014 20:15:00 -0600',
-                            author: 'The Czar',
-                            title: 'Test Number 3',
-                            url: 'http://www.northstarhoopsreport.com/news_article/show/460716?referral=rss&referrer_id=982824',
-                            articleID: 460716,
-                            paywalled: false,
-                            tags: []
-                        }]
-                }]
-        };
-
-        console.log("Full article with site and date: ", newDateArticle);
-
-        //$http({
-        //    url: '/api/getObjectID',
-        //    method: 'GET',
-        //    params: {date: dbDate, siteID: 1}
-        //    }).then(function(response){
-        //    console.log(response);
-        //});
-
-        $http.post('/api/articleAdd', newDateArticle).then(function(response) {
-            console.log("Your article was added successfully! ", response);
-        });
-
-    };
-
-    //working on modal to list articles on specific date
-
-    $scope.animationsEnabled = true;
-    $scope.openModal = function(size){
-        $scope.selectedArticles = this.day.site[0].articles;
-        var modalInstance = $modal.open(
-            {
-                animation: $scope.animationsEnabled,
-                templateUrl: '/assets/views/routes/articleUrl.html',
-                controller: 'ArticleInstanceController',
-                size: size,
-                resolve: {
-                    selectedArticles: function(){
-                        return $scope.selectedArticles;
-                    }
-                }
-            }
-        )
-    }
-
-}]);
-
-prepHoopsApp.controller('ArticleInstanceController', ['$scope', '$modalInstance', 'selectedArticles', function($scope, $modalInstance, selectedArticles){
-    $scope.modalArticles = selectedArticles;
-    console.log($scope.modalArticles);
-
-    for(var i = 0; i < $scope.modalArticles.length; i++){
-        console.log($scope.modalArticles[i].url);
-    }
-
-    $scope.ok = function () {
-        $modalInstance.close();
-    };
+//    var myDate = new Date('9/13/2015');
+//    console.log(myDate);
+//    var year = myDate.getFullYear().toString();
+//    var tempMonth = myDate.getMonth();
+//    var month = (tempMonth + 1).toString();
+//    var day = myDate.getDate().toString();
+//    var dbDate = year + month + day;
+//    console.log(dbDate);
+//
+//    $scope.testDatabase = function() {
+//        var newDateArticle =
+//        {
+//            date: dbDate,
+//            site:
+//                [{
+//                    siteName: 'North Star Hoops Report',
+//                    siteID: 1,
+//                    articles:
+//                        [{
+//                            pubDate: 'Sat, 27 Dec 2014 20:15:00 -0600',
+//                            author: 'The Czar',
+//                            title: 'Test Number 3',
+//                            url: 'http://www.northstarhoopsreport.com/news_article/show/460716?referral=rss&referrer_id=982824',
+//                            articleID: 460716,
+//                            paywalled: false,
+//                            tags: []
+//                        }]
+//                }]
+//        };
+//
+//        console.log("Full article with site and date: ", newDateArticle);
+//
+//        //$http({
+//        //    url: '/api/getObjectID',
+//        //    method: 'GET',
+//        //    params: {date: dbDate, siteID: 1}
+//        //    }).then(function(response){
+//        //    console.log(response);
+//        //});
+//
+//        $http.post('/api/articleAdd', newDateArticle).then(function(response) {
+//            console.log("Your article was added successfully! ", response);
+//        });
+//
+//    };
+//
+//    //working on modal to list articles on specific date
+//
+//    $scope.animationsEnabled = true;
+//    $scope.openModal = function(size){
+//        $scope.selectedArticles = this.day.site[0].articles;
+//        var modalInstance = $modal.open(
+//            {
+//                animation: $scope.animationsEnabled,
+//                templateUrl: '/assets/views/routes/articleUrl.html',
+//                controller: 'ArticleInstanceController',
+//                size: size,
+//                resolve: {
+//                    selectedArticles: function(){
+//                        return $scope.selectedArticles;
+//                    }
+//                }
+//            }
+//        )
+//    }
+//
+//}]);
+//
+//prepHoopsApp.controller('ArticleInstanceController', ['$scope', '$modalInstance', 'selectedArticles', function($scope, $modalInstance, selectedArticles){
+//    $scope.modalArticles = selectedArticles;
+//    console.log($scope.modalArticles);
+//
+//    for(var i = 0; i < $scope.modalArticles.length; i++){
+//        console.log($scope.modalArticles[i].url);
+//    }
+//
+//    $scope.ok = function () {
+//        $modalInstance.close();
+//    };
 }]);
