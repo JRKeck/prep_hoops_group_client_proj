fe5dc44d477d683719a86267c2a9bd84a0c05390
Bob Fix <bobfix@gmail.com>
2015-09-25 13:13:06 -0500

    Added date creation to ParseRSS.js - refactored logic for execution of events requiring a sequence.

--- server/routes/blankSite.js
+++ server/routes/blankSite.js
@@ -7,61 +7,91 @@ var path = require('path');
 var Feeds = require('../models/rssdb');
 var Articles = require('../models/articledb');
 var ParseDate = require('../models/parseDate');
+var rssFeeds = [];
 
-// Need to get the current last parse Date
-function getDate(){
-    ParseDate.find({}, function(err, lastDate){
-        if(err) {
-            console.log("Error in pulling last parse date: ", err);
-        }
-        var lastParse = lastDate[0].date;
-        var currentDate = new Date();
-        var startDate = new Date("September 20, 2015 01:00:00");
-        console.log(currentDate - startDate);
-        //var a = currentDate.setDate(currentDate.getDate() -30);
-        //var newDate = new Date(a);
-        var month = new Array();
-        month[0] = "-01-";
-        month[1] = "-02-";
-        month[2] = "-03-";
-        month[3] = "-04-";
-        month[4] = "-05-";
-        month[5] = "-06-";
-        month[6] = "-07-";
-        month[7] = "-08-";
-        month[8] = "-09-";
-        month[9] = "-10-";
-        month[10] = "-11-";
-        month[11] = "-12-";
 
-        var lastParseDayTemp = (lastParse.getDate()).toString();
-        if (lastParseDayTemp < 10) {
-            var lastParseDay = "0" + lastParseDayTemp;
+// Need to get the last article collection date from the database
+function dateCollectionUpdate(rssFeeds) {
+    Articles.find({}).sort({date: -1}).limit(1).exec(function (err, lastdate) {
+        if (err) {
+            console.log("Error pulling articles: ", err);
         } else {
-            var lastParseDay = lastParseDayTemp;
-        }
-        var lastParseMonth = month[lastParse.getMonth()];
-        var lastParseYear = ((lastParse.getYear()) + 1900).toString();
-        var currentDateDay = (currentDate.getDate()).toString();
-        var currentDateMonth = month[currentDate.getMonth()];
-        var currentDateYear = ((currentDate.getYear()) + 1900).toString();
-        var startDateDayTemp = (startDate.getDate()).toString();
-        if (startDateDayTemp < 10) {
-            var startDateDay = "0" + startDateDayTemp;
-        } else {
-            var startDateDay = startDateDayTemp;
-        }
-        var startDateMonth = month[startDate.getMonth()];
-        var startDateYear = ((startDate.getYear()) + 1900).toString();
-        console.log("Last Parse Date: ", lastParse);
-        console.log("Current Date: ", currentDate);
-        //console.log("Value of a: ", a);
-        //console.log("New Date: ", newDate);
-        console.log("Start Date String: " + startDateYear + startDateMonth + startDateDay);
-        console.log("Last Parse String: " + lastParseYear + lastParseMonth + lastParseDay);
-        console.log("Current Date String: " + currentDateYear + currentDateMonth + currentDateDay);
+            // Format last article collection date in Date for comparison to current date
+            var tempLastParseDate = new Date(lastdate[0].date);
+            var lastParseDate = new Date(tempLastParseDate.setDate(tempLastParseDate.getDate() + 1));
+            var MS_PER_DAY = 1000 * 60 * 60 * 24;
+            var currentDate = new Date();
+            console.log("Last Article Date Colleciton: ", lastParseDate);
+            console.log("Current Date: ", currentDate);
 
+            // a and b are javascript Date objects
+            function dateDiffInDays(a, b) {
+                // Discard the time and time-zone information.
+                var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+                var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
+                return Math.floor((utc2 - utc1) / MS_PER_DAY);
+            }
+
+            var daysSinceLastParse = dateDiffInDays(lastParseDate, currentDate);
+            console.log("Days Since Last Parse: ", daysSinceLastParse);
+
+            var month = new Array();
+            month[0] = "-01-";
+            month[1] = "-02-";
+            month[2] = "-03-";
+            month[3] = "-04-";
+            month[4] = "-05-";
+            month[5] = "-06-";
+            month[6] = "-07-";
+            month[7] = "-08-";
+            month[8] = "-09-";
+            month[9] = "-10-";
+            month[10] = "-11-";
+            month[11] = "-12-";
+
+            if (daysSinceLastParse > 0) {
+                for (var i = 0; i < daysSinceLastParse; i++) {
+                    console.log("Current Date is: ", currentDate);
+                    var currentDateDayTemp = (currentDate.getDate()).toString();
+                    if (currentDateDayTemp < 10) {
+                        var currentDateDay = "0" + currentDateDayTemp;
+                    } else {
+                        var currentDateDay = currentDateDayTemp;
+                    }
+                    var currentDateMonth = month[currentDate.getMonth()];
+                    var currentDateYear = ((currentDate.getYear()) + 1900).toString();
+                    var currentDateString = currentDateYear + currentDateMonth + currentDateDay;
+                    console.log("Current Date String: " + currentDateString);
+
+                    var dateArticleToAdd = {
+                        date: currentDateString,
+                        site: []
+                    };
+                    console.log("Preparing to create sites in date: " + currentDateString);
+                    console.log("Number of sites to add: ", rssFeeds.length);
+                    for (var j = 0; j < rssFeeds.length; j++) {
+                        var sitePush = {
+                            siteName: rssFeeds[j].siteFullName,
+                            siteID: rssFeeds[j].siteID,
+                            siteShortName: rssFeeds[j].siteShortName,
+                            articles: []
+                        };
+                        dateArticleToAdd.site.push(sitePush);
+                    }
+                    console.log(dateArticleToAdd);
+                    //Articles.create(dateArticleToAdd, function (err, post) {
+                    //    if (err) {
+                    //        console.log("Error on Article Create: ", err);
+                    //    } else {
+                    //        console.log(post);
+                    //    }
+                    //});
+                    currentDate = new Date(currentDate.setDate(currentDate.getDate() - 1));
+                    console.log("Current Date at end of loop: ", currentDate)
+                }
+            }
+        }
     });
 }
 
@@ -71,43 +101,17 @@ function getFeeds() {
         if (err) {
             console.log("Error in pull sites from database ", err);
         }
-        console.log(sites);
+        //console.log(sites);
         console.log("Number of Sites: ", sites.length);
-        for(var j = 15; j < 20; j++) {
-            var temp = j.toString();
-            var dateArticleToAdd = {
-                date: "2015-09-" + temp,
-                site: []
-            };
-            for (var i = 0; i < sites.length; i++) {
-                var sitePush = {
-                    siteName: sites[i].siteFullName,
-                    siteID: sites[i].siteID,
-                    siteShortName: sites[i].siteShortName,
-                    articles: []
-                };
-                dateArticleToAdd.site.push(sitePush);
-            }
-            console.log(dateArticleToAdd);
-            //Articles.create(dateArticleToAdd, function (err, post) {
-            //    if (err) {
-            //        console.log("Error on Article Create: ", err);
-            //    } else {
-            //        console.log(post);
-            //    }
-            //});
-        }
+        rssFeeds = sites;
+        getDate(rssFeeds)
     });
 }
 
 router.get('/*', function(req, res, next){
-    console.log("Get Date!");
-    getDate();
-    console.log('Get Sites!');
-    //getFeeds();
-    console.log('Getting Sites Complete!');
+    console.log("Get Sites!");
+    getFeeds();
     res.send('Getting Sites Complete!');
-
 });
 
 module.exports = router;
